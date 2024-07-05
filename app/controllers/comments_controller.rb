56723f092b2b85ac98927284d4a28bf80f1513e7
NecmiyeSoylu <necmiyesoyla@gmail.com>
2024-07-05 13:13:15 +0300

    destroy method is made comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+    # http_basic_authenticate_with name:"ns",password:"ns_p", only:destroy
     def create 
         @article = Article.find(params[:article_id])
         @comment=@article.comments.create(comment_params)
@@ -6,12 +7,12 @@ class CommentsController < ApplicationController
     end
 
    
-    def destroy
-        @article = Article.find.(params[:article_id])
-        @comment = @article.comments.find(params[:id])
-        @comment.destroy
-        redirect_to article_path(@article)
-    end
+    # def destroy
+    #     @article = Article.find.(params[:article_id])
+    #     @comment = @article.comments.find(params[:id])
+    #     @comment.destroy
+    #     redirect_to article_path(@article)
+    # end
 
     # def show
     #     @article = Article.find.(params[:article_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   resources :articles do
-    resources :comments
+    resources :comments, only: %i[index new create]
   end
   root 'welcome#index'
 end
