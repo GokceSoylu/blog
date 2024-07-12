cf3ed463dbec381920286d91a36d662f898db578
NecmiyeSoylu <necmiyesoyla@gmail.com>
2024-07-12 11:44:59 +0300

    validation errors fixed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,7 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+
     def create
         @article = Article.new(article_params)
         if @article.save
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
     has_many :comments, dependent: :destroy #bu sayaede makale silindiği zaman makalenin yorumlarıda silinecek
-    validates :title,  presence:true, 
+    validates :title,  presence: true, 
     length: {minimum: 2}
 end
 
